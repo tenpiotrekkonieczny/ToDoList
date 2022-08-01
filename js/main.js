8f3a9a49838a9583cd90136b6b473f4bb1c56123
Piotr Konieczny <piotrekprogramista@gmail.com>
2022-08-01 22:38:36 +0200

    make enter key to add todos and completion of the project

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ const prepareDOMElements = () => {
   uList = document.querySelector('.todolist ul');
 
   popup = document.querySelector('.popup');
-  popupInfo = document.querySelector('popup-info');
+  popupInfo = document.querySelector('.popup-info');
   popupInput = document.querySelector('.popup-input');
   popupAddBtn = document.querySelector('.accept');
   popupCloseBtn = document.querySelector('.cancel');
@@ -32,6 +32,8 @@ const prepareDOMEvents = () => {
   addBtn.addEventListener('click', addNewToDo);
   uList.addEventListener('click', checkClick);
   popupCloseBtn.addEventListener('click', closePopup);
+  popupAddBtn.addEventListener('click', changeTodoText);
+  todoInput, addEventListener('keyup', enterKeyCheck);
 };
 
 const addNewToDo = () => {
@@ -74,18 +76,48 @@ const checkClick = (e) => {
     e.target.closest('li').classList.toggle('completed');
     e.target.classList.toggle('completed');
   } else if (e.target.matches('.edit')) {
-    editTodo();
+    editTodo(e);
   } else if (e.target.matches('.delete')) {
-    console.log('delete');
+    deleteTodo(e);
   }
 };
 
-const editTodo = () => {
+const editTodo = (e) => {
+  todoToEdit = e.target.closest('li');
+
+  popupInput.value = todoToEdit.firstChild.textContent;
   popup.style.display = 'flex';
 };
 
 const closePopup = () => {
   popup.style.display = 'none';
+  popupInfo.textContent = '';
+};
+
+const changeTodoText = () => {
+  if (popupInput.value !== '') {
+    todoToEdit.firstChild.textContent = popupInput.value;
+    popup.style.display = 'none';
+    popupInfo.textContent = '';
+  } else {
+    popupInfo.textContent = 'Musisz podać jakąś wartość!';
+  }
+};
+
+const deleteTodo = (e) => {
+  e.target.closest('li').remove();
+
+  const allTodos = uList.querySelectorAll('li');
+
+  if (allTodos.length === 0) {
+    errorInfo.textContent = 'Brak zadań na liście.';
+  }
+};
+
+const enterKeyCheck = (e) => {
+  if (e.key === 'Enter') {
+    addNewToDo();
+  }
 };
 
 document.addEventListener('DOMContentLoaded', main);
