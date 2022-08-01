2899eeb3362ade899e7d0f92564fa1d2995e58f7
Piotr Konieczny <piotrekprogramista@gmail.com>
2022-08-01 21:32:02 +0200

    add popup

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@ let todoInput; // miejsce do wpisywania zadania
 let errorInfo; // info o braku zadania
 let addBtn; // przycisk do dodawania zadania
 let uList; // lista zadań
-// let newTodo; // nowe zadanie
+let newTodo; // nowe zadanie
+
+let popup; // popup
+let popupInfo; // tekst w popupie, jak się doda pusty text
+let todoToEdit; // edytoany todo
+let popupInput; // input w popupie
+let popupAddBtn; // przycisk "zatwierdź" w popupie
+let popupCloseBtn; // przycisk "anuluj w popupie"
 const main = () => {
   prepareDOMElements();
   prepareDOMEvents();
@@ -13,10 +20,18 @@ const prepareDOMElements = () => {
   errorInfo = document.querySelector('.error-info');
   addBtn = document.querySelector('.btn-add');
   uList = document.querySelector('.todolist ul');
+
+  popup = document.querySelector('.popup');
+  popupInfo = document.querySelector('popup-info');
+  popupInput = document.querySelector('.popup-input');
+  popupAddBtn = document.querySelector('.accept');
+  popupCloseBtn = document.querySelector('.cancel');
 };
 
 const prepareDOMEvents = () => {
   addBtn.addEventListener('click', addNewToDo);
+  uList.addEventListener('click', checkClick);
+  popupCloseBtn.addEventListener('click', closePopup);
 };
 
 const addNewToDo = () => {
@@ -52,6 +67,25 @@ const createToolsArea = (newTodo) => {
   deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
 
   toolsPanel.append(completeBtn, editBtn, deleteBtn);
+};
+
+const checkClick = (e) => {
+  if (e.target.matches('.complete')) {
+    e.target.closest('li').classList.toggle('completed');
+    e.target.classList.toggle('completed');
+  } else if (e.target.matches('.edit')) {
+    editTodo();
+  } else if (e.target.matches('.delete')) {
+    console.log('delete');
+  }
+};
+
+const editTodo = () => {
+  popup.style.display = 'flex';
+};
+
+const closePopup = () => {
+  popup.style.display = 'none';
 };
 
 document.addEventListener('DOMContentLoaded', main);
